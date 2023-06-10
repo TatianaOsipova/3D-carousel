d9f3d9243f79548423f4ab8745e752d6693419e7
Tatiana Osipova <otv120780@gmail.com>
2023-06-10 18:36:02 -0400

    feat: add soundbutton

--- main.js
+++ main.js
@@ -114,3 +114,22 @@ document.onmousewheel = function(e) {
   radius += d;
   init(1);
 };
+
+// Audio
+
+let soundButton = document.querySelector('.soundbutton'),
+audio = document.querySelector('.audio')
+
+soundButton.addEventListener('click', e => {
+    soundButton.classList.toggle('paused')
+    audio.paused ? audio.play() : audio.pause()
+}) 
+
+window.onfocus = function() {
+    soundButton.classList.contains('paused') ? audio.pause() : audio.play()
+}
+
+window.onblur = function() {
+    audio.pause()
+}
+
